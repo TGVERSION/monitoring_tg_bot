97d0013db1e0c0dd60c298f7122defe8b9f4fd24
TGVERSION <yaroslavtsev6464@mail.ru>
2026-04-27 14:06:47 +0500

    fix: use config vars in scheduler startup log

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, REPORT_DAY, REPORT_TIME
 from handlers import admin, registration
 from scheduler import create_scheduler
 
@@ -20,7 +20,7 @@ async def main() -> None:
 
     scheduler = create_scheduler(bot)
     scheduler.start()
-    logging.info("Scheduler started. Report day: %s, time: %s", "monday", "09:30")
+    logging.info("Scheduler started. Report day: %s, time: %s", REPORT_DAY, REPORT_TIME)
 
     try:
         await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
